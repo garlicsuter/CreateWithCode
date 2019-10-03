5094851639f9c1880e9dcc21a5a0b2042c55c26d
garlicsuter <markasuter@gmail.com>
2019-10-02 20:18:07 -0400

    obstacle and bg moves

---
+++ Prototype_3_JumpForce/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
--- Prototype_3_JumpForce/Assets/Scripts/PlayerController.cs
+++ Prototype_3_JumpForce/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            // Four Types of ForceMode - 1: Acceleration, 2:Force (continuous force based on mass),
+            //  3: Impulse (uses mass, instant),and 4: VelocityChange (Ignores Mass)
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
