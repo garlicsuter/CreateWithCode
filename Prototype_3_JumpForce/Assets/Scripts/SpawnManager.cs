c4a067d51ead6ee0c0b3df0b2d94a0baa4554d14
garlicsuter <markasuter@gmail.com>
2019-10-04 19:51:59 -0400

    3.1 complete

--- Prototype_3_JumpForce/Assets/Scripts/SpawnManager.cs
+++ Prototype_3_JumpForce/Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
