7e62f33e55ff9ed49cb0da41af16b3f4d9dcccc1
garlicsuter <markasuter@gmail.com>
2019-10-04 20:04:43 -0400

    repeatbackground script added

---
+++ Prototype_3_JumpForce/Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < startPos.x - 40)
+        {
+            transform.position = startPos;
+        }
+    }
+}
