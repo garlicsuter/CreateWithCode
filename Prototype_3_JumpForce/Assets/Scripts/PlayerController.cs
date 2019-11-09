fe2029a79bc0aaf382ba2a55520bfbe820f15e04
garlicsuter <markasuter@gmail.com>
2019-11-09 11:23:11 -0500

    boom!

--- Prototype_3_JumpForce/Assets/Scripts/PlayerController.cs
+++ Prototype_3_JumpForce/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +43,8 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType", 1);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
         
     }
