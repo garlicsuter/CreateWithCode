3f91d01beb0a3e4aff28ac82fbe0511d48dfc3e1
garlicsuter <markasuter@gmail.com>
2019-11-12 19:00:56 -0500

    Add Force + TextMesh Pro

---
+++ Prototype_3_JumpForce/Assets/Lab3Greyboxed/Scripts/PlayerControllerL3AddForce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerL3AddForce : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+
+        Debug.Log(verticalInput + " Vertical Input");
+        Debug.Log(horizontalInput + " Horizontal Input");
+    }
+}
