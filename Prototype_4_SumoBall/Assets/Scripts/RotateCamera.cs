f6c03f97155d677601cfce0a826804222d014424
garlicsuter <markasuter@gmail.com>
2019-11-16 10:38:13 -0500

    roll-a-ball deluxe

--- Prototype_4_SumoBall/Assets/Scripts/RotateCamera.cs
+++ Prototype_4_SumoBall/Assets/Scripts/RotateCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    public float rotationSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class RotateCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0,1,0), horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
