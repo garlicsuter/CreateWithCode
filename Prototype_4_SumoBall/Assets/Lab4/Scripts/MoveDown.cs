69007aaebf41c11185c798ccb0a4c939b44b310d
Mark Suter <msuter@live.elida.k12.oh.us>
2019-11-25 10:12:39 -0500

    lab 4 updates

---
+++ Prototype_4_SumoBall/Assets/Lab4/Scripts/MoveDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody objectRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectRb.AddForce(Vector3.forward * -speed);
+    }
+}
