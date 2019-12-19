9a81aaeb8181e73079e4638e6029f950ef0640e9
garlicsuter <markasuter@gmail.com>
2019-12-19 08:23:34 -0500

    Challenge 5 complete

---
+++ Prototype5-ClickyCrate-UI/Assets/Challenge 5/Scripts/CountdownTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float timeLeft = 10.0f;
+    public Text text;
+
+    public GameManagerX gmX;
+
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left: " + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            gmX.GameOver();
+        }
+    }
+}
